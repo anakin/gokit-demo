2781d2eb72f6f888ce8c3691bb65109e3ae8727d
Anakin Sun <anakin.sun@chope.co>
2019-07-12 11:38:50 +0800

    init

---
+++ demo1/go.mod
@@ -0,0 +1,11 @@
+module demo1
+
+go 1.12
+
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/go-kit/kit v0.9.0
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/prometheus/common v0.6.0
+)
---
+++ demo1/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/go-kit/kit/endpoint"
+	ht "github.com/go-kit/kit/transport/http"
+	"github.com/prometheus/common/log"
+	"net/http"
+)
+
+type HelloService interface {
+	Hello(string) (string, error)
+}
+
+type helloService struct {
+}
+
+func (h helloService) Hello(s string) (string, error) {
+	if s == "" {
+		return "", errors.New("no param")
+	}
+	return "hello " + s, nil
+}
+
+type helloReponse struct {
+	V   string `json:"s"`
+	Err string `json:"err,omitempty"`
+}
+
+type helloRequest struct {
+	S string `json:"s"`
+}
+
+func makeHelloEndpoint(svc HelloService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(helloRequest)
+		v, err := svc.Hello(req.S)
+		if err != nil {
+			return helloReponse{"", err.Error()}, nil
+		}
+		return helloReponse{v, ""}, nil
+	}
+}
+
+func decodeHelloRequest(_ context.Context,r *http.Request)(interface{},error)  {
+var req helloRequest
+if err:=json.NewDecoder(r.Body).Decode(&req);err !=nil{
+	return nil,err
+}
+return req,nil
+}
+
+func encodeResponse(_ context.Context,w http.ResponseWriter,resp interface{}) error{
+	return json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+	svc:=helloService{}
+	helloHandler:=ht.NewServer(
+		makeHelloEndpoint(svc),
+		decodeHelloRequest,
+		encodeResponse,
+		)
+	http.Handle("/hello",helloHandler)
+	log.Fatal(http.ListenAndServe(":8080",nil))
+}
